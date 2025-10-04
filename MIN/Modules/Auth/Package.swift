f730fc92bb78b8d77a34ca967cafd07f599626d2
Stanislav Shalgin <s.shalgin@gmail.com>
2025-10-04 14:27:42 +0300

    Add Firebase dependencies

--- MIN/Modules/Auth/Package.swift
+++ MIN/Modules/Auth/Package.swift
@@ -5,15 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "Auth",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         .library(
             name: "Auth",
             targets: ["Auth"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.3.0")
+    ],
     targets: [
         .target(
             name: "Auth",
+            dependencies: [
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk"),
+            ],
             path: "Sources"
         ),
     ]
