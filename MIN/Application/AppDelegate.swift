8c451c4278ea42cbabafcacdba1c1c736e23dc02
Stanislav Shalgin <s.shalgin@gmail.com>
2025-10-04 11:02:33 +0300

    Project settings

---
+++ MIN/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//  Created by Stanislav Shalgin on 29.09.2025.
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    // MARK: - UIApplicationDelegate
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
+
